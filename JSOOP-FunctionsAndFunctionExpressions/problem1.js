d7f7d053d34fbb115841c3ff2fbdd274e7b9a7f0
tomihristov5 <hrist0v@abv.bg>
2015-06-21 13:57:39 +0300

    final

---
+++ JSOOP-FunctionsAndFunctionExpressions/problem1.js
@@ -0,0 +1,24 @@
+﻿// Task 1
+// Write a function that sums an array of numbers:
+// numbers must be always of type Number
+// returns `null` if the array is empty
+// throws Error if the parameter is not passed (undefined)
+// throws if any of the elements is not convertible to Number	
+
+function sum(arr) {
+    if (arr === undefined) {
+        throw new Error('Enter an array!');
+    } else if (!arr.length) {
+        return null;
+    } else {
+        if (!arr.every(function (item) {
+                return !isNaN(item);
+        })) {
+            throw new Error('All elements must be convertible to numbers.');
+        }
+    }
+
+    return arr.reduce(function (result, item) {
+        return result + +item;
+    }, 0);
+}
---
+++ JSOOP-FunctionsAndFunctionExpressions/problem2.js
@@ -0,0 +1,38 @@
+﻿// Task 2
+// Write a function a function that finds all the prime numbers in a range
+// 1) it should return the prime numbers in an array
+// 2) it must throw an Error if any on the range params is not convertible to `string`
+// 3) it must throw an Error if any of the range params is missing
+
+function solve(x, y) {
+    if (arguments.length < 2) {
+        throw 'Error! The funciton needs 2 arguments!';
+    } else if (isNaN(arguments[0]) || isNaN(arguments[1])) {
+        throw 'Error! Arguments must be convertible to numbers.';
+    }
+    var result = [],
+        i,
+        j,
+        maxDivisor,
+        isPrime;
+    x *= 1;
+    y *= 1;
+
+    for (i = x; i <= y; i += 1) {
+        maxDivisor = Math.sqrt(i);
+        isPrime = true;
+        if (i < 2) {
+            isPrime = false;
+        }
+        for (j = 2; j <= maxDivisor; j += 1) {
+            if (!(i % j)) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            result.push(i);
+        }
+    }
+    return result;
+}
